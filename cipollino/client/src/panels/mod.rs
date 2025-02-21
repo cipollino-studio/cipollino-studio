e6937a8b2318e205aca57d192a23a898357e32a3
Nikos1001 <nikos@plugachev.com>
2025-02-21 02:07:37 -0500

    Added panel add dropdown

--- cipollino/client/src/panels/mod.rs
+++ cipollino/client/src/panels/mod.rs
@@ -39,8 +39,13 @@ impl pierro::DockingTab for EditorPanel {
         self.panel.render(ui, state); 
     }
 
-    fn add_tab_dropdown<F: FnMut(Self)>(_ui: &mut pierro::UI, _add_tab: F, _context: &mut EditorState) {
-
+    fn add_tab_dropdown<F: FnMut(Self)>(ui: &mut pierro::UI, mut add_tab: F, _context: &mut EditorState) {
+        if pierro::menu_button(ui, "Assets").mouse_clicked() {
+            add_tab(EditorPanel::new::<AssetsPanel>());
+        }
+        if pierro::menu_button(ui, "Scene").mouse_clicked() {
+            add_tab(EditorPanel::new::<ScenePanel>());
+        }
     }
 
 }
