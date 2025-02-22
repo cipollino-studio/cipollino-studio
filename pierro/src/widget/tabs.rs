676f93988fd03efe1d7549d89b0b8bba2038a484
Nikos1001 <nikos@plugachev.com>
2025-02-22 18:36:50 -0500

    Added animation to tab close button

--- pierro/src/widget/tabs.rs
+++ pierro/src/widget/tabs.rs
@@ -1,6 +1,6 @@
 use crate::{icons, Layout, Margin, Response, Size, UINodeParams, UI};
 
-use super::{button_fill_animation, h_spacing, icon_text_style, label, Theme};
+use super::{button_fill_animation, clickable_icon, h_spacing, label, Theme};
 
 pub struct TabResponse {
     pub tab: Response,
@@ -12,7 +12,6 @@ pub fn tab<S: Into<String>>(ui: &mut UI, label_text: S, selected: bool) -> TabRe
     let theme = ui.style::<Theme>();
     let tab_bg = if selected { theme.bg_light } else { theme.bg_dark };
     let widget_margin = theme.widget_margin;
-    let icon_style = icon_text_style(ui);
 
     let (tab, close_button) = ui.with_node(
         UINodeParams::new(Size::fit(), Size::fit())
@@ -24,13 +23,7 @@ pub fn tab<S: Into<String>>(ui: &mut UI, label_text: S, selected: bool) -> TabRe
             label(ui, label_text);
             h_spacing(ui, 6.0);
 
-            let close_button = ui.node(
-                UINodeParams::new(Size::text(), Size::text())
-                    .with_text(icons::X)
-                    .sense_mouse()
-                    .with_text_style(icon_style)
-            );
-            close_button
+            clickable_icon(ui, icons::X)
         }
     );
 
