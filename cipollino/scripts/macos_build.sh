87e54b3082d73f225f3a4e1fb9a845f3c9976bfe
Nikos1001 <nikos@plugachev.com>
2025-02-25 02:09:04 -0500

    Added mac bundling script

---
+++ cipollino/scripts/macos_build.sh
@@ -0,0 +1,9 @@
+
+cargo build --bin cipollino-studio --release --target aarch64-apple-darwin
+
+mkdir -p ../build/Cipollino.app/Contents/MacOS
+mkdir -p ../build/Cipollino.app/Contents/Resources
+
+cp ../target/aarch64-apple-darwin/release/cipollino-studio ../build/Cipollino.app/Contents/MacOS/Cipollino
+cp client/res/macos/icon.icns ../build/Cipollino.app/Contents/Resources/icon.icns
+cp client/res/macos/Info.plist ../build/Cipollino.app/Contents/
