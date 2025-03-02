eb17ae08b51c57c69ecb4878ced2f84285d9a01d
Nikos1001 <nikos@plugachev.com>
2025-03-02 12:04:14 -0500

    [cipollino] Fixed clip creation undo bug

--- cipollino/client/src/panels/assets/menu_bar.rs
+++ cipollino/client/src/panels/assets/menu_bar.rs
@@ -1,5 +1,5 @@
 
-use project::{Action, Clip, Folder, Ptr};
+use project::{alisa::Action, Clip, ClipTreeData, CreateClip, Folder, Ptr};
 
 use crate::ProjectState;
 
@@ -17,11 +17,33 @@ impl AssetsPanel {
         }
     }
 
+    fn create_clip(state: &ProjectState) {
+        let Some(clip_ptr) = state.client.next_ptr() else { return; };
+        let Some(inner_ptr) = state.client.next_ptr() else { return; };
+        state.client.queue_action(Action::single(CreateClip {
+            ptr: clip_ptr,
+            parent: Ptr::null(),
+            data: ClipTreeData {
+                name: "Clip".to_owned(),
+                length: 100,
+                framerate: 24.0,
+                inner_ptr,
+                ..Default::default()
+            },
+        }));
+    }
+    
+    fn clip_menu_bar_icon(&self, ui: &mut pierro::UI, state: &ProjectState) {
+        if pierro::icon_button(ui, Clip::ICON).mouse_clicked() {
+            Self::create_clip(state);
+        }
+    } 
+
     pub(crate) fn menu_bar(&self, ui: &mut pierro::UI, state: &ProjectState) {
         pierro::menu_bar(ui, |ui| {
             self.asset_menu_bar_icon::<Folder>(ui, state); 
-            self.asset_menu_bar_icon::<Clip>(ui, state); 
-        });   
+            self.clip_menu_bar_icon(ui, state);
+        });
     }
 
 }
